da53838d04261a64f37a809cc3bc6a0050c5f2b0
wpppl <w-f108@163.com>
2015-02-03 22:34:34 +0800

    生成训练集

--- xlj.py
+++ xlj.py
@@ -1,16 +1,18 @@
 # -*- coding: cp936 -*-
 fp1 = open("www.txt",'r')
 fp = open("3d_xlj.txt","w")
+i=0
 for line in fp1 :
     #print(line[31])#机号
     #print(line[33])#球号
     #line=fp1.readline()
     #print(line)
    
-    line1=line[0:25]
+    line1=line[19:25]
     print(line1)
     
-    fp.write(line1+'\n')   
+    fp.write(str(i)+':'+line1+'\n')
+    i=i+1
         
                 
 fp.close()
